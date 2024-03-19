d203b369b286e9f63cf9280e40496a3f4f3a89e2
sfjt <sho.foujita@gmail.com>
2024-03-19 12:16:06 +0900

    type fix

--- src/server/middlewares/checkExpiredToken.ts
+++ src/server/middlewares/checkExpiredToken.ts
@@ -1,8 +1,8 @@
 import { Response, Request, NextFunction, RequestHandler } from "express"
 
 export function checkExpiredToken(url: string): RequestHandler {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    const { accessToken } = req.oidc
+  return async (req: any, res: Response, next: NextFunction) => {
+    const accessToken = req?.oidc.accessToken
     if (accessToken?.isExpired()) {
       res.render("./expiredToken.ejs", {
         accessToken: accessToken?.access_token || "None",
